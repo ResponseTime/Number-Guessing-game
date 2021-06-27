ae65afeaf1c44a8b951df5e73ff0648218e82427
Looper <aayush.bhattacharjee2002@gmail.com>
2021-06-28 00:18:22 +0530

    Made changes in controller and made new ui2 file

--- number/controller.java
+++ number/controller.java
@@ -1,8 +1,27 @@
 package number;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class controller {
     @FXML
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+    public void change(ActionEvent e) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ui2.fxml"));
+        root = loader.load();
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
