82dccbef371f3a6f1eaa4b52cea669af99eee6e5
Looper <aayush.bhattacharjee2002@gmail.com>
2021-06-28 00:31:23 -0700

    Added feature of keeping a track of the user

--- number/controller.java
+++ number/controller.java
@@ -21,11 +21,28 @@ public class controller {
     TextField user;
     @FXML
     PasswordField pass;
-
+    int k = 1;
+    int l = 1;
+    String array [] = new String [k];
+    String arr [] = new String[l];
    
     public void change(ActionEvent e) throws IOException{
         String username = user.getText();
         String password = pass.getText();
+        array = new String[k++];
+        for(int i =0;i<array.length;i++){
+            array[i] = username;
+        }
+        arr = new String[l++];
+        for(int j = 0;j<arr.length;j++){
+            arr[j]= password;
+        }
+        for(String r:array){
+            System.out.println(r);
+        }
+        for(String t:arr){
+            System.out.println(t);
+        }
         FXMLLoader loader = new FXMLLoader(getClass().getResource("ui2.fxml"));
         root = loader.load();
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
