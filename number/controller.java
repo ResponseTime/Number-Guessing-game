6fd0f8208968b0b023898abba4a4a9fa9983e552
Looper <aayush.bhattacharjee2002@gmail.com>
2021-06-28 17:08:05 +0530

    Fixed a bug in database

--- number/controller.java
+++ number/controller.java
@@ -42,7 +42,7 @@ public class controller {
                 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3308/aayush", "root","root");
                 System.out.println("connected");
                 Statement st = con.createStatement();
-                String query  = "INSERT INTO LOGIN_INFO VALUES('"+username+"','"+password+"')";
+                String query  = "INSERT INTO LOGIN_INFO(USERNAME,PASSWORD) VALUES('"+username+"','"+password+"')";
                 st.executeUpdate(query);
                    
                
