5e0b0eaa9aa903ea1b9708dd23dcfee943cf051b
Looper <aayush.bhattacharjee2002@gmail.com>
2021-06-28 03:22:33 -0700

    Completed the database connection of login page

--- number/controller.java
+++ number/controller.java
@@ -32,7 +32,7 @@ public class controller {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/aayush", "root","root");
             System.out.println("connected");
             Statement st = con.createStatement();
-            String query = "INSERT INTO LOGIN_INFO"+"VALUES(%s,%s)"+username+password;
+            String query = "INSERT INTO LOGIN_INFO"+"VALUES('"+username+"','"+password+"')";
             st.executeUpdate(query);
         }
         catch(Exception ev){
